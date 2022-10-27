c8de9da9c7ef3a9de0a917c1e1e0ca15fd7cfe8d
Miguel Urena <urena_miguel82@outlook.com>
2022-10-27 14:26:43 -0700

    Fixed bugs in index.html and styles.css

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 import Deck from "./deck";
 
+const computerCardSlot = document.querySelector('.computer-card-slot')
+
 const deck = new Deck()
 deck.shuffle()
 console.log(deck.cards)
+
+computerCardSlot.appendChild(deck.cards[0].getHTML())
