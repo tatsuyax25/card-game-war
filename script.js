87579cf4aa756c181013654b290cbd30b353dc4a
Miguel Urena <urena_miguel82@outlook.com>
2022-10-28 14:28:30 -0700

    Update updateDeckCount

--- script.js
+++ script.js
@@ -46,6 +46,8 @@ function flipCards() {
 
     playerCardSlot.appendChild(playerCard.getHTML())
     computerCardSlot.appendChild(computerCard.getHTML())
+
+    updateDeckCount()
 }
 
 function updateDeckCount() {
