47421b766b11d8e64cc401ebacc3dd8ab78ec3a5
Miguel Urena <urena_miguel82@outlook.com>
2022-10-28 14:07:49 -0700

    Add more function to script.js

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 import Deck from "./deck.js";
 
 const computerCardSlot = document.querySelector(".computer-card-slot")
+const playerCardSlot = document.querySelector(".player-card-slot")
+const computerDeckElement = document.querySelector('.computer-deck')
+const playerDeckElement = document.querySelector('.player-deck')
+const text = document.querySelector('.text')
 
 let playerDeck, computerDeck
 
@@ -13,6 +17,11 @@ function startGame() {
     playerDeck = new Deck(deck.cards.slice(0, deckMidpoint))
     computerDeck = new Deck(deck.cards.slice(deckMidpoint, deck.numberOfCards))
 
-    console.log(playerDeck)
-    console.log(computerDeck)
+    cleanBeforeRound()
+}
+
+function cleanBeforeRound() {
+    computerCardSlot.innerHTML = ''
+    playerCardSlot.innerHTML = ''
+    text.innerText = ''
 }
