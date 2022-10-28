532c6b887e0c71971110f36d30d505b695dbd0b0
Miguel Urena <urena_miguel82@outlook.com>
2022-10-28 14:36:50 -0700

    Add winning condition

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 import Deck from "./deck.js";
 
+const CARD_VALUE_MAP = {
+    "2": 2,
+    "3": 3,
+    "4": 4,
+    "5": 5,
+    "6": 6,
+    "7": 7,
+    "8": 8,
+    "9": 9,
+    "10": 10,
+    "J": 11,
+    "Q": 12,
+    "K": 13,
+    "A": 14
+}
+
+
 const computerCardSlot = document.querySelector(".computer-card-slot")
 const playerCardSlot = document.querySelector(".player-card-slot")
 const computerDeckElement = document.querySelector('.computer-deck')
@@ -48,9 +65,17 @@ function flipCards() {
     computerCardSlot.appendChild(computerCard.getHTML())
 
     updateDeckCount()
+
+    if (isRoundWinner(playerCard, computerCard)) {
+        text.innerText = "Win"
+    }
 }
 
 function updateDeckCount() {
     computerDeckElement.innerText = computerDeck.numberOfCards
     playerDeckElement.innerText = playerDeck.numberOfCards
+}
+
+function isRoundWinner(cardOne, cardTwo) {
+    return CARD_VALUE_MAP[cardOne.value] > CARD_VALUE_MAP[cardTwo.value]
 }
